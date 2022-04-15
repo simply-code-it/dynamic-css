8ee7078d07a0d49c680d1ab63515fe4870be7cbb
pradeep <dashwar.pradeep@gmail.com>
2022-04-15 15:27:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [content, setContent] = useState("Naga");
+  const [pad, setPad] = useState(7);
+  const [fontt, setFontt] = useState(10);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>
+          <span>Content : </span>
+          <input
+            type="text"
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+            placeholder="enter content"
+          />
+        </div>
+        <div>
+          <span>Padding : </span>
+          <input
+            type="number"
+            onChange={(e) => {
+              setPad(e.target.value);
+            }}
+            placeholder="enter number"
+          />
+        </div>
+        <div>
+          <span>fontSize : </span>
+          <input
+            type="number"
+            onChange={(e) => {
+              setFontt(e.target.value);
+            }}
+            placeholder="enter number"
+          />
+        </div>
+      </div>
+      <div
+        style={{
+          padding: `${pad}px`,
+          fontSize: `${fontt}px`,
+          border: "1px solid black",
+          width: "20%",
+          margin: "auto",
+        }}
+      >
+        <h2>{content}</h2>
+      </div>
     </div>
   );
 }
